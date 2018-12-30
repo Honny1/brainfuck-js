3ab4055996348268e4b4d60ac87d5f648874acab
Hony Rodák <hony.com@seznam.cz>
2018-12-30 02:00:38 +0100

    Change for my needs

--- js/brainfuck.js
+++ js/brainfuck.js
@@ -1,183 +1,129 @@
-var parse = (function () {
-  var input;
-  var output;
-  var data;
-  var ptr;
-  var debug = false;
-
-  var ops = {
-    '+': function () {
-      data[ptr] = data[ptr] || 0;
-      data[ptr]++;
-      debug && console.log('+', data[ptr], ptr);
-    },
-
-    '-': function () {
-      data[ptr] = data[ptr] || 0;
-      data[ptr]--;
-      debug && console.log('-', data[ptr], ptr);
-    },
-
-    '<': function () {
-      ptr--;
-      if (ptr < 0) {
-        ptr = 0; //Don't allow pointer to leave data array
-      }
-      debug && console.log('<', ptr);
-    },
-
-    '>': function () {
-      ptr++;
-      debug && console.log('>', ptr);
-    },
-
-    '.': function () {
-      var c = String.fromCharCode(data[ptr]);
-      output.push(c);
-      debug && console.log('.', c, data[ptr]);
-    },
-
-    ',': function () {
-      var c = input.shift();
-      if (typeof c == "string") {
-        data[ptr] = c.charCodeAt(0);
-      }
-      debug && console.log(',', c, data[ptr]);
-    },
-  };
-
-  function program(nodes) {
-    return function (inputString) {
-      output = [];
-      data = [];
-      ptr = 0;
-
-      input = inputString && inputString.split('') || [];
-
-      nodes.forEach(function (node) {
-        node();
-      });
-
-      return output.join('');
-    }
-  }
-
-  function loop(nodes) {
-    return function () {
-      var loopCounter = 0;
-
-      while(data[ptr] > 0) {
-        if (loopCounter++ > 10000) { throw "Infinite loop detected"; }
-
-        nodes.forEach(function (node) {
-          node();
-        });
-      }
-    };
-  }
-
-
-
-  var programChars;
-
-  function parseProgram() {
-    var nodes = [];
-    var nextChar;
-
-    while (programChars.length > 0) {
-      nextChar = programChars.shift();
-      if (ops[nextChar]) {
-        nodes.push(ops[nextChar]);
-      } else if (nextChar == '[') {
-        nodes.push(parseLoop());
-      } else if (nextChar == ']') {
-        throw "Missing opening bracket";
-      } else {
-        // ignore it
-      }
-    }
-
-    return program(nodes);
-  }
-
-  function parseLoop() {
-    var nodes = [];
-    var nextChar;
-
-    while (programChars[0] != ']') {
-      nextChar = programChars.shift();
-      if (nextChar == undefined) {
-        throw "Missing closing bracket";
-      } else if (ops[nextChar]) {
-        nodes.push(ops[nextChar]);
-      } else if (nextChar == '[') {
-        nodes.push(parseLoop());
-      } else {
-        // ignore it
-      }
-    }
-    programChars.shift(); //discard ']'
-
-    return loop(nodes);
-  }
-
-  function parse(str) {
-    programChars = str.split('');
-    return parseProgram();
-  }
-
-  return parse;
-})();
-
-
-function run(code, input) {
-  return parse(code)(input);
+function start (code, input) {
+  var interpreter = new Brainfuck(code)
+  return interpreter.execute(input)
 }
 
+function Brainfuck (code) {
+    // data
+    this.i = null
+    this.data = null
+    // output
+    this.input = null
+    this.output = null
+    // source code
+    this.c = null
+    this.source = code
 
-$(document).ready(function () {
-  function makeUrl() {
-    var code = $('#code').val() || '';
-    var input = $('#input').val() || '';
-    var url = 'http://skilldrick.co.uk/brainfuck/';
-    url += '?code=' + code;
-    url += '&input=' + encodeURIComponent(input);
-    $('#url').attr('href', url);
-  }
+    // maps ['s to their ]'s
+    this.blockOpeners = {}
+    // maps ]'s back to their ['s
+    this.blockClosers = {}
+    // populates the block index maps
+    this.getBlocks()
+}
 
-  var queryString = window.location.search.substring(1);
-  var paramsArray = queryString.split('&');
-  var params = {};
-  for (var i = 0; i < paramsArray.length; i++) {
-    var param = paramsArray[i].split('=');
-    params[param[0]] = decodeURI(param[1]);
-  }
+Brainfuck.prototype = new function () {
+    this.getBlocks = function () {
+        var code = this.source
+        var i = 0
+        var l = code.length
+        var match, open
+        var brackets = /\[|\]/g
+        var stack = []
+        while (i < code.length) {
+            match = code.substring(i).search(brackets)
+            if (match < 0)
+                break
+            
+            match += i
+            
+            if (code[match] === '[') {
+                stack.push(match)
+            } else {
+                open = stack.pop()
+                this.blockOpeners[open] = match
+                this.blockClosers[match] = open
+            }
 
-  $('#code').val(params.code);
-  $('#input').val(params.input);
-  makeUrl();
-
-
-  $('#code, #input').change(function () {
-    makeUrl();
-  });
-
-  $('form').submit(function (e) {
-    e.preventDefault();
-    var code = $('#code').val();
-    var input = $('#input').val();
-    var output;
-    try {
-      output = run(code, input);
+            i = match + 1
+        }
+        if (stack.length)
+            throw 'Brackets mismatched!'
     }
-    catch (e) {
-      output = e;
-    }
-    $('#output').text(output);
-  });
-});
 
-var output = run('++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.');
+    this.execute = function (input) {
+        this.c = 0
+        this.i = 0
+        this.data = [0]
+        this.output = ''
+        this.input = input
+        var source = this.source
+        var l = source.length
+        while (this.c < l) {
+            this[source[this.c]]()
+        }
+        return this.output
+    }
+
+    // operations
+    this['>'] = function () {
+        this.i++
+        if (this.i === this.data.length)
+            this.data[this.i] = 0
+        this.c++
+    }
+    
+    this['<'] = function () {
+        this.i--
+        if (this.i < 0) 
+            this.i = 0
+        this.c++
+    }
+    
+    this['+'] = function () {
+        this.data[this.i]++
+
+        if (this.data[this.i] === 256)
+            this.data[this.i] = 0
+        this.c++
+    }
+    
+    this['-'] = function () {
+        this.data[this.i]--
+        if (this.data[this.i] === -1)
+            this.data[this.i] = 255
+        this.c++
+    }
+    
+    this['.'] = function () {
+        this.output += String.fromCharCode(this.data[this.i])
+        this.c++
+    }
+    
+    this[','] = function () {
+        if (!this.input.length)
+            throw 'Input expected!'
+        this.data[this.i] = this.input.charCodeAt(0)
+        this.input = this.input.substring(1)
+        this.c++
+    }
+    
+    this['['] = function () {
+        if (!this.data[this.i])
+            this.c = this.blockOpeners[this.c] + 1
+        else
+            this.c++
+    }
+    
+    this[']'] = function () {
+        if (this.data[this.i])
+            this.c = this.blockClosers[this.c] + 1
+        else
+            this.c++
+    }
+}
+
+/*var output = start('++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.');
 console.log(output);
-output = run(',[.-]', 'Z');
-console.log(output);
+output = start(',[.-]', 'Z');
+console.log(output);*/
